9d3820b32569ef81d1613a872e78abf703cb510f
kostyacha <KostyaCh2010@hotmail.com>
2024-12-27 21:56:41 +0400

    almost Finished Paint

--- paint/MainWindow.xaml.cs
+++ paint/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace paint
 
         private bool IsDrawing;
         private int currentTool = 0;
+        private int size = 1;
+        private Brush color;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +34,14 @@ namespace paint
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             IsDrawing = true;
+            if (currentTool == 0 )
+            {
+                Draw(e.GetPosition(CnvPaint));
+            }
+            if (currentTool == 1)
+            {
+                Erase(e.GetPosition(CnvPaint));
+            }
         }
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
@@ -45,19 +55,53 @@ namespace paint
             {
                 Draw(e.GetPosition(CnvPaint));
             }
+            if (currentTool == 1)
+            {
+                Erase(e.GetPosition(CnvPaint));
+            }
+
+
         }
         private void Draw(Point position)
         {
             Ellipse elipse = new Ellipse();
-            elipse.Fill = Brushes.Aqua;
-            elipse.Width = 10;
-            elipse.Height = 10;
-            Canvas.SetTop(elipse, position.Y);
-            Canvas.SetLeft(elipse, position.X);
-            CnvPaint.Children.Add(elipse);
+            elipse.Fill = color;
+            elipse.Width = size;
+            elipse.Height = size;
+            Canvas.SetTop(elipse, position.Y -  elipse.Height / 2);
+            Canvas.SetLeft(elipse, position.X - elipse.Width / 2 );
+            CnvPaint.Children.Add(elipse); 
+
 
 
         }
+        private void Erase(Point position)
+        {
+            IsDrawing = false ;
+
+            Rect erase = new Rect();
+            erase.Width = size;
+            erase.Height = size;
+            Point Place = new Point(position.X - erase.Width / 2, position.Y - erase.Height);
+            erase.Location = Place; 
+            for     (int i = 0; i < CnvPaint.Children.Count; i++)
+            {
+                Rect point = new Rect();
+                point.Size = CnvPaint.Children[i].RenderSize;
+                point.Location = new Point(
+                 Canvas.GetLeft(CnvPaint.Children[i]),
+                 Canvas.GetTop(CnvPaint.Children[i])
+                 );
+
+                if (erase.IntersectsWith(point))
+                {
+                    CnvPaint.Children.RemoveAt(i);
+                    i--;
+                }
+            }
+
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -85,6 +129,46 @@ namespace paint
                 SliderSize.Visibility = Visibility.Visible;
                 ColorPicker.Visibility = Visibility.Visible; 
             }
+
+        }
+
+        private void SliderSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            size = (int)SliderSize.Value;
+        }
+
+        private void ColorPicker_Selected(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void ColorPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+               switch(ColorPicker.SelectedIndex)
+            {
+                case 0:
+                    color = Brushes.Black;
+
+                    break;
+                    case 1:
+                    color =Brushes.Red;
+
+                     break;
+                    case 2:
+                    color = Brushes.Green;
+
+                    break;
+                    case 3:
+                    color = Brushes.Blue;
+                    break;
+                    default:
+                    color = Brushes.Black;
+
+                    break;
+
+            }
+               
 
         }
     }
